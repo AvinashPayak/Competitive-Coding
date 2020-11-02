6fc842a805a88280d4a7c28b936a93fd7619253d
AvinashPayak <avinashpayak@gmail.com>
2020-11-02 23:43:19 +0530

    stairways

---
+++ Avinash/unsolved/salem.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.lang.Math;
+class Solution {
+    public static void main(String args[]){
+        Scanner pp = new Scanner(System.in);
+            int n = pp.nextInt();
+            int[] arr = new int[n];
+            for(int i = 0;i<n;i++){
+                arr[i] = pp.nextInt();
+            }
+
+            Arrays.sort(arr);
+            for(int i=1;i<n;i++){
+                arr[]
+            }
+            
+        pp.close();
+    }
+
+}
